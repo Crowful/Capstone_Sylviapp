a5d7e8264777e0e07a22722bc380e6c8ab4e863a
softfietea <tribaco.alfie@gmail.com>
2021-09-01 17:50:30 +0800

    Parentdatawidget error Fixed

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -40,68 +40,66 @@ class _HomePageState extends State<HomePage> {
                   : BorderRadius.circular(0)),
           child: SingleChildScrollView(
             padding: EdgeInsets.all(10),
-            child: Expanded(
-              child: Column(
-                children: [
-                  SizedBox(height: 35),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      !menuOpen
-                          ? IconButton(
-                              icon: Icon(Icons.menu),
-                              onPressed: () {
-                                setState(() {
-                                  widget.controller.forward();
-                                  menuOpen = true;
-                                });
-                              },
-                              color: Color(0xff403d55),
-                            )
-                          : IconButton(
-                              icon: Icon(Icons.arrow_back_ios),
-                              onPressed: () {
-                                setState(() {
-                                  widget.controller.reverse();
-                                  menuOpen = false;
-                                });
-                              },
-                              color: Color(0xff403d55),
-                            ),
-                      IconButton(
-                        icon: Icon(Icons.notifications),
-                        onPressed: null,
-                        color: Colors.blueAccent,
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 15),
-                  Container(
-                    height: 200,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(color: Colors.black26),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text('Campaigns',
-                      style: GoogleFonts.roboto(fontWeight: FontWeight.bold)),
-                  Container(
-                    height: 200,
-                    width: 200,
-                    child: ListView.builder(
-                        itemCount: 5,
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                            height: 120,
-                            width: 120,
-                            child: Text("Test"),
-                          );
-                        }),
-                  ),
-                ],
-              ),
+            child: Column(
+              children: [
+                SizedBox(height: 35),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    !menuOpen
+                        ? IconButton(
+                            icon: Icon(Icons.menu),
+                            onPressed: () {
+                              setState(() {
+                                widget.controller.forward();
+                                menuOpen = true;
+                              });
+                            },
+                            color: Color(0xff403d55),
+                          )
+                        : IconButton(
+                            icon: Icon(Icons.arrow_back_ios),
+                            onPressed: () {
+                              setState(() {
+                                widget.controller.reverse();
+                                menuOpen = false;
+                              });
+                            },
+                            color: Color(0xff403d55),
+                          ),
+                    IconButton(
+                      icon: Icon(Icons.notifications),
+                      onPressed: null,
+                      color: Colors.blueAccent,
+                    )
+                  ],
+                ),
+                SizedBox(height: 15),
+                Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(color: Colors.black26),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text('Campaigns',
+                    style: GoogleFonts.roboto(fontWeight: FontWeight.bold)),
+                Container(
+                  height: 200,
+                  width: 200,
+                  child: ListView.builder(
+                      itemCount: 5,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          height: 120,
+                          width: 120,
+                          child: Text("Test"),
+                        );
+                      }),
+                ),
+              ],
             ),
           ),
         ),
