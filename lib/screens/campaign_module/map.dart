98cfd14b478175518e7b2d8fb4fcf77a138b0e9d
Crowful <joshdpj@gmail.com>
2021-09-25 17:25:25 +0800

    pash pash sept 25

--- lib/screens/campaign_module/map.dart
+++ lib/screens/campaign_module/map.dart
@@ -9,13 +9,127 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  bool clicked = false;
   final _initialCameraPosition =
       CameraPosition(target: LatLng(14.5995, 120.9842));
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(initialCameraPosition: _initialCameraPosition),
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(children: [
+          GoogleMap(
+              zoomControlsEnabled: false,
+              initialCameraPosition: _initialCameraPosition),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    AnimatedOpacity(
+                      duration: Duration(milliseconds: 500),
+                      opacity: clicked ? 1 : 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            color: Colors.transparent),
+                        height: 50,
+                        width: MediaQuery.of(context).size.width,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    clicked = false;
+                                  });
+                                },
+                                child: Container(
+                                    height: 50,
+                                    width: 100,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(5))),
+                                    child: Center(
+                                        child: Text('Angat\nWatershed')))),
+                            GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    clicked = false;
+                                  });
+                                },
+                                child: Container(
+                                    height: 50,
+                                    width: 100,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(5))),
+                                    child: Center(
+                                        child: Text('La Mesa \nWatershed')))),
+                            GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    clicked = false;
+                                  });
+                                },
+                                child: Container(
+                                    height: 50,
+                                    width: 100,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(5))),
+                                    child: Center(
+                                        child: Text('La Mesa \nWatershed')))),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          clicked = true;
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: Offset(0, 4),
+                              ),
+                            ],
+                            color: Color(0xff65BFB8)),
+                        height: 50,
+                        width: MediaQuery.of(context).size.width,
+                        child: Center(
+                          child: Text(
+                            'Create Campaign',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
+            ),
+          )
+        ]),
+      ),
     );
   }
+
+  moveToAngat() {}
 }
