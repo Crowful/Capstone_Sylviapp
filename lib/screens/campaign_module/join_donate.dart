1dfd0e80d8a50176e37c0683d832f5040a1e8ef4
softfietea <tribaco.alfie@gmail.com>
2021-11-04 02:19:29 +0800

    update of joining campaign

--- lib/screens/campaign_module/join_donate.dart
+++ lib/screens/campaign_module/join_donate.dart
@@ -8,7 +8,16 @@ import 'package:sylviapp_project/screens/campaign_module/volunteer_form.dart';
 
 class JoinDonateCampaign extends StatefulWidget {
   final String? uidOfCampaign;
-  const JoinDonateCampaign({Key? key, this.uidOfCampaign}) : super(key: key);
+  final String? address;
+  final String? description;
+  final String? uidOfOrganizer;
+  const JoinDonateCampaign(
+      {Key? key,
+      this.uidOfCampaign,
+      this.address,
+      this.description,
+      this.uidOfOrganizer})
+      : super(key: key);
 
   @override
   _JoinDonateCampaignState createState() => _JoinDonateCampaignState();
@@ -19,6 +28,7 @@ class _JoinDonateCampaignState extends State<JoinDonateCampaign> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.white.withOpacity(0.97),
         body: Center(
           child: StreamBuilder<DocumentSnapshot>(
               stream: FirebaseFirestore.instance
@@ -29,105 +39,194 @@ class _JoinDonateCampaignState extends State<JoinDonateCampaign> {
                 if (!snapshot.hasData) {
                   return CircularProgressIndicator();
                 } else {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                          margin: EdgeInsets.fromLTRB(0, 30, 270, 50),
-                          child: Column(children: [
-                            Text(
-                              snapshot.data!.get("campaign_name"),
-                              style: TextStyle(
-                                  color: Color(0xff65BFB8),
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.w600),
-                            ),
-                            Text("address"),
-                          ])),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(20, 0, 20, 10),
-                        child: Text("23/50" + " Volunteers"),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(20, 0, 20, 10),
-                        child: Text(
-                            "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(20, 0, 20, 10),
-                        child: Text("Organizer Name: " + "Alfie C. Tribaco"),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                        child: Text("Requirements: "),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                        child: Text("Vaccination ID "),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                        child: Text("Medical Record"),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                        child: Text("Planting Tools "),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                        child: Text("Proper Gears "),
-                      ),
-                      SizedBox(
-                        height: 150,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                  return Stack(fit: StackFit.expand, children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 462.5),
+                      height: 300,
+                      width: 350,
+                      child: Image.network(
+                          "https://images.unsplash.com/photo-1425913397330-cf8af2ff40a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80"),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(30, 230, 30, 10),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      width: 320,
+                      height: 400,
+                      child: Column(
                         children: [
-                          ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  primary: Colors.green),
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            VolunteerFormScreen()));
-                              },
-                              child: Text("Join Campaign")),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  primary: Colors.orange),
-                              onPressed: () async {
-                                var request = BraintreeDropInRequest(
-                                    tokenizationKey:
-                                        'sandbox_mf5kvmgw_mhmfxcfrgwwftpcq',
-                                    collectDeviceData: true,
-                                    paypalRequest: BraintreePayPalRequest(
-                                      amount: '10.00',
-                                      displayName: 'SylviaApp',
+                          Container(
+                              child: Column(children: [
+                            Row(children: [
+                              Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      width: 300,
+                                      margin: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                                      child: Text(
+                                        "Angat Campaign",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 25,
+                                            fontWeight: FontWeight.w900),
+                                      ),
                                     ),
-                                    cardEnabled: true);
+                                    Container(
+                                        width: 150,
+                                        margin:
+                                            EdgeInsets.fromLTRB(20, 5, 0, 0),
+                                        child: Text(
+                                          "Tagaytay City",
+                                          style: TextStyle(
+                                              color: Colors.black54,
+                                              fontSize: 11),
+                                        )),
+                                  ]),
+                            ]),
+                          ])),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
+                            child: Text(
+                                "I'm Alfie Tribaco the organizer of this campaign and I would like to make this campaign success as possible, come and join to this campaign and make our nature be better again.",
+                                style: TextStyle(color: Colors.black54)),
+                          ),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Column(children: [
+                                    Text(
+                                      "23/50",
+                                      style: TextStyle(
+                                          fontSize: 30,
+                                          fontWeight: FontWeight.w900),
+                                    ),
+                                    Text("Volunteers",
+                                        style: TextStyle(color: Colors.black54))
+                                  ]),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  Column(children: [
+                                    Text(
+                                      " ₱1400",
+                                      style: TextStyle(
+                                          fontSize: 30,
+                                          fontWeight: FontWeight.w900),
+                                    ),
+                                    Text("Fund Needed",
+                                        style: TextStyle(color: Colors.black54))
+                                  ]),
+                                ]),
+                          ),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 120,
+                                  child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          primary: Colors.orange,
+                                          shape: StadiumBorder(
+                                              side: BorderSide(
+                                                  width: 1,
+                                                  color: Colors.orange))),
+                                      onPressed: () async {
+                                        var request = BraintreeDropInRequest(
+                                            tokenizationKey:
+                                                'sandbox_mf5kvmgw_mhmfxcfrgwwftpcq',
+                                            collectDeviceData: true,
+                                            paypalRequest:
+                                                BraintreePayPalRequest(
+                                              amount: '10.00',
+                                              displayName: 'SylviaApp',
+                                            ),
+                                            cardEnabled: true);
 
-                                BraintreeDropInResult? result =
-                                    await BraintreeDropIn.start(request);
+                                        BraintreeDropInResult? result =
+                                            await BraintreeDropIn.start(
+                                                request);
 
-                                if (result != null) {
-                                  print(result.paymentMethodNonce.description);
-                                  print(result.paymentMethodNonce.nonce);
-                                } else {
-                                  print("FAILED PAYMENT PROCESS");
-                                }
-                              },
-                              child: Text("Donate to this Campaign")),
+                                        if (result != null) {
+                                          print(result
+                                              .paymentMethodNonce.description);
+                                          print(
+                                              result.paymentMethodNonce.nonce);
+                                        } else {
+                                          print("FAILED PAYMENT PROCESS");
+                                        }
+                                      },
+                                      child: Text(
+                                        "D O N A T E",
+                                        style: TextStyle(color: Colors.white),
+                                      )),
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Container(
+                                  width: 120,
+                                  child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          primary: Color(0xff65BFB8),
+                                          shape: StadiumBorder()),
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    VolunteerFormScreen()));
+                                      },
+                                      child: Text("J O I N ")),
+                                ),
+                              ],
+                            ),
+                          )
                         ],
-                      )
-                    ],
-                  );
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(50, 600, 50, 20),
+                      width: 200,
+                      height: 200,
+                      color: Colors.white54,
+                      child: ExpansionTile(
+                        backgroundColor: Colors.white54,
+                        title: Text("Terms And Conditions"),
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                  "This is the Terms and Conditions that should be followed by the volunteer, during and after the campaign."),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 0, 330, 680),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                          size: 35,
+                        ),
+                      ),
+                    )
+                  ]);
                 }
               }),
         ),
--- lib/screens/campaign_module/volunteer_form.dart
+++ lib/screens/campaign_module/volunteer_form.dart
@@ -38,49 +38,80 @@ class _VolunteerFormScreenState extends State<VolunteerFormScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-      children: [
-        Text("SUBMIT FORM"),
-        ElevatedButton(
-          child: Text("SUBMIT MEDICAL FORM"),
-          onPressed: () {},
-        ),
-        Text("SAFETY CHECKLIST"),
-        Row(children: [
-          Checkbox(value: true, onChanged: (value) {}),
-          Text("HELMET")
-        ]),
-        Row(children: [
-          Checkbox(value: true, onChanged: (value) {}),
-          Text("GAMOT")
-        ]),
-        Row(children: [
-          Checkbox(value: true, onChanged: (value) {}),
-          Text("PLASTIC")
-        ]),
-        Row(children: [
-          Checkbox(value: true, onChanged: (value) {}),
-          Text("GAMOT")
-        ]),
-        Row(children: [
-          Checkbox(value: true, onChanged: (value) {}),
-          Text("GLOVES")
-        ]),
-        Container(
-          height: 200,
-          width: 300,
-          child: Row(children: [
+    return SafeArea(
+      child: Scaffold(
+          body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 20, 150, 0),
+            child: Text(
+              "FILL UP FORM",
+              style: TextStyle(
+                  color: Color(0xff65BFB8),
+                  fontSize: 30,
+                  fontWeight: FontWeight.w900),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(15, 20, 15, 0),
+            child: Text(
+              "As a Volunteer you need to submit the following in this form, also, you need to consider to bring the following safety gears and essentials items during the campaigns",
+              style: TextStyle(color: Colors.black54),
+            ),
+          ),
+          Container(
+            width: 180,
+            child: ElevatedButton(
+              child: Row(
+                  children: [Icon(Icons.attach_file), Text(" VACCINATION ID")]),
+              onPressed: () {},
+            ),
+          ),
+          Container(
+            width: 210,
+            child: ElevatedButton(
+              child: Row(children: [
+                Icon(Icons.attach_file),
+                Text(" MEDICAL CERTIFICATE")
+              ]),
+              onPressed: () {},
+            ),
+          ),
+          Text("SAFETY CHECKLIST"),
+          Row(children: [
             Checkbox(value: true, onChanged: (value) {}),
-            Text(
-              "I hereby accept the terms and conditions in this campaign, I will plant keme and I will follow the rules on this campaign",
-              softWrap: true,
-            )
+            Text("HELMET")
           ]),
-        ),
-        ElevatedButton(
-            onPressed: () {}, child: Text("Submit Information to Organizer"))
-      ],
-    ));
+          Row(children: [
+            Checkbox(value: true, onChanged: (value) {}),
+            Text("GAMOT")
+          ]),
+          Row(children: [
+            Checkbox(value: true, onChanged: (value) {}),
+            Text("PLASTIC")
+          ]),
+          Row(children: [
+            Checkbox(value: true, onChanged: (value) {}),
+            Text("GAMOT")
+          ]),
+          Row(children: [
+            Checkbox(value: true, onChanged: (value) {}),
+            Text("GLOVES")
+          ]),
+          Container(
+            height: 200,
+            width: 300,
+            child: Row(children: [
+              Checkbox(value: true, onChanged: (value) {}),
+              Text(
+                "I hereby accept the terms and conditions in this campaign, I will plant keme and I will follow the rules on this campaign",
+                softWrap: true,
+              )
+            ]),
+          ),
+          ElevatedButton(onPressed: () {}, child: Text("JOIN"))
+        ],
+      )),
+    );
   }
 }
