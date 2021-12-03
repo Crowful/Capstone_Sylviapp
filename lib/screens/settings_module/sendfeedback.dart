adfc1cbee6446f8c7ade820383c621e493b342cd
Crowful <joshdpj@gmail.com>
2021-12-03 16:36:16 +0800

    layouts

--- lib/screens/settings_module/sendfeedback.dart
+++ lib/screens/settings_module/sendfeedback.dart
@@ -36,55 +36,96 @@ class _SendFeedbackScreenState extends State<SendFeedbackScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-              margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
-              child: Text(
-                "Send Feedback",
-                style: TextStyle(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.arrow_back_ios,
                     color: Color(0xff65BFB8),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25),
-              )),
-          Container(
-              margin: EdgeInsets.fromLTRB(20, 0, 20, 40),
-              child: Text(
-                "Send feedback to the application",
-                style: TextStyle(color: Colors.black54),
-              )),
-          Container(
-              margin: EdgeInsets.fromLTRB(20, 0, 20, 10),
-              child: TextField(
-                maxLines: 5,
-                controller: _feedbackController,
-                inputFormatters: [LengthLimitingTextInputFormatter(100)],
-              )),
-          Center(
-            child: Container(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    shape: StadiumBorder(), primary: Colors.green),
-                onPressed: () {
-                  String dateCreated =
-                      formatDate(DateTime.now(), [yyyy, '-', mm, '-', dd]);
-                  if (_feedbackController.text == "") {
-                    Fluttertoast.showToast(msg: 'Please Input feedback');
-                  } else {
-                    context.read(authserviceProvider).addFeedback(
-                        _feedbackController.text,
-                        context.read(authserviceProvider).getCurrentUserUID(),
-                        dateCreated);
-                    _feedbackController.clear();
-                  }
-                },
-                child: Text("Submit"),
-              ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  color: Color(0xff403d55),
+                ),
+                Text(
+                  'Sylviapp',
+                  style: TextStyle(
+                      color: Color(0xff65BFB8),
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold),
+                ),
+                IconButton(
+                  icon: Icon(Icons.bookmark_outline),
+                  onPressed: () {},
+                  color: Colors.transparent,
+                ),
+              ],
             ),
-          )
-        ],
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              "Send Feedback",
+              style: TextStyle(
+                  color: Color(0xff65BFB8),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25),
+            ),
+            Text(
+              "Send feedback to the application, limit of 100 characters only!",
+              style: TextStyle(color: Colors.grey),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.2),
+                    borderRadius: BorderRadius.all(Radius.circular(5))),
+                child: TextField(
+                  maxLines: 5,
+                  controller: _feedbackController,
+                  inputFormatters: [LengthLimitingTextInputFormatter(100)],
+                )),
+            SizedBox(
+              height: 10,
+            ),
+            GestureDetector(
+              onTap: () {
+                String dateCreated =
+                    formatDate(DateTime.now(), [yyyy, '-', mm, '-', dd]);
+                if (_feedbackController.text == "") {
+                  Fluttertoast.showToast(msg: 'Please Input feedback');
+                } else {
+                  context.read(authserviceProvider).addFeedback(
+                      _feedbackController.text,
+                      context.read(authserviceProvider).getCurrentUserUID(),
+                      dateCreated);
+                  _feedbackController.clear();
+                }
+              },
+              child: Container(
+                height: 40,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Color(0xff65BFB8),
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                child: Center(
+                  child: Text("Submit"),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     ));
   }
