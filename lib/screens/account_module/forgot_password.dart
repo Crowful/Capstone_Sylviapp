37d62ece23743352bb999d972701a370883e83dd
softfietea <tribaco.alfie@gmail.com>
2022-07-03 22:43:38 +0800

    UI fixed for forgot Password

--- lib/screens/account_module/forgot_password.dart
+++ lib/screens/account_module/forgot_password.dart
@@ -29,78 +29,74 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          padding: EdgeInsets.all(18),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              Text("Forgot Password",
-                  style: TextStyle(
-                      color: Color(0xff65BFB8),
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold)),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Enter your email and we will email you a form for your new password',
-                style: GoogleFonts.openSans(
-                    fontSize: 13, fontWeight: FontWeight.w600),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                child: TextFormField(
-                  controller: _emailController,
-                  onChanged: (email) => onEmailChanged(email),
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.black)),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.black)),
-                    hintText: "Email",
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Center(
-                child: Container(
-                  height: 60,
-                  width: 300,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          primary: Color(0xff65BFB8), shape: StadiumBorder()),
-                      onPressed: () {
-                        if (_emailController.text == "") {
-                          Fluttertoast.showToast(
-                              msg: 'The Email must not be blank');
-                        } else {
-                          context
-                              .read(authserviceProvider)
-                              .resetPass(_emailController.text);
-
-                          _emailController.clear();
-                        }
-                      },
-                      child: Text("Send Request Password to my Email")),
-                ),
-              )
-            ],
+    return Container(
+      padding: EdgeInsets.all(18),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 20,
           ),
-        ),
+          Text("Forgot Password",
+              style: TextStyle(
+                  color: Color(0xff65BFB8),
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold)),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Enter your email and we will email you a form for your new password',
+            style:
+                GoogleFonts.openSans(fontSize: 13, fontWeight: FontWeight.w600),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            child: TextFormField(
+              controller: _emailController,
+              onChanged: (email) => onEmailChanged(email),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.black)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.black)),
+                hintText: "Email",
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: Container(
+              height: 60,
+              width: 300,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Color(0xff65BFB8), shape: StadiumBorder()),
+                  onPressed: () {
+                    if (_emailController.text == "") {
+                      Fluttertoast.showToast(
+                          msg: 'The Email must not be blank');
+                    } else {
+                      context
+                          .read(authserviceProvider)
+                          .resetPass(_emailController.text);
+
+                      _emailController.clear();
+                    }
+                  },
+                  child: Text("Send Request Password to my Email")),
+            ),
+          )
+        ],
       ),
     );
   }
